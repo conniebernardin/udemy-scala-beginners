f7cf106f6b0611750a3a28f656164709a47c3b17
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-03 10:18:59 +0100

    call by name and call by value notes

--- src/main/scala/lectures/part1basics/CBNvsCBV.scala
+++ src/main/scala/lectures/part1basics/CBNvsCBV.scala
@@ -2,4 +2,27 @@ package lectures.part1basics
 
 object CBNvsCBV extends App{
 
+//long value type is usually used for time in nanoseconds
+  def calledByValue(x: Long): Unit ={
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+  //exact value of the expression is computed before the function evaluates, then the same value is used in the function
+  //definition
+
+  //arrow tells you that the function will be called by name
+  def calledByName(x: => Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+  //expression is called as is, and is evaluated every time
+  //useful in lazy streams and things that might fail
+  
+  calledByValue(System.nanoTime()) //two identical values
+  calledByName(System.nanoTime()) //two different values
+
+//demonstrating by name function
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
 }
