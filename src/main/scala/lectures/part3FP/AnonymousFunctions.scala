ddfdc8d51cb0af5ce16739063291862f5244189c
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-12 11:31:23 +0100

    Anonymous Functoins exercises

--- src/main/scala/lectures/part3FP/AnonymousFunctions.scala
+++ src/main/scala/lectures/part3FP/AnonymousFunctions.scala
@@ -2,4 +2,19 @@ package lectures.part3FP
 
 object AnonymousFunctions extends App {
 
+  //anonymous function [LAMBDA]
+  val doubler: Int => Int = (x: Int) => x*2
+
+  val adder: (Int, Int) => Int = (a: Int, b:Int) => a+b
+
+  //Using curly brackets with Lambdas
+
+  val strintToInt = { (s: String) =>
+    s.toInt
+  }
+  val niceAdder: (Int, Int) => Int = _+_
+
+  val superAdd = (x: Int) => (y: Int) => x+y
+  println(superAdd(3) (4)) //curried
+
 }
