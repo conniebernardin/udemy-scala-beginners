f2344b41973ef58796fbd47d7c6a9ab66944710d
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-12 15:02:33 +0100

    maps filters notes

--- src/main/scala/lectures/part3FP/MapFlatmapFilterFor.scala
+++ src/main/scala/lectures/part3FP/MapFlatmapFilterFor.scala
@@ -2,4 +2,20 @@ package lectures.part3FP
 
 object MapFlatmapFilterFor extends App {
 
+  val list = List(1,2,3)
+  println(list.head)
+  println(list.tail)
+
+
+  //map
+  println(list.map(_ + 1))
+  println(list.map(_ + " is a number"))
+
+  //filter
+  println(list.filter(_ % 2 == 0))
+
+  //flatmap
+  val toPair = (x: Int) => List(x, x+1)
+  println(list.flatMap(toPair))
+
 }
